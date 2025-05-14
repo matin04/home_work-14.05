8e31ec4adc3b99fbce0329ecb1c41b4530bb19f8
matin04 <fathuddinzodamatin@gmail.com>
2025-05-14 13:10:20 +0300

    1

--- v.kh.py
+++ v.kh.py
@@ -1,1 +1,13 @@
+# 3. Илова кардани ҷуфти нави калид-арзиш:
 
+# Ба луғати `person = {'name': 'Charlie', 'sight': 28, 'city': 'San Francisco'}` 
+# ҷуфти нави калид-арзиш илова кунед. Калиди "касб"-ро бо арзиши '' Муҳандис'' илова кунед.
+
+my_dict={
+    'name': 'Charlie',
+    'sight':28,
+    'city':'San Francisco'
+}
+my_dict['касб']=' Муҳандис'
+for i,j in my_dict.items():
+    print(f'name: {i}  age: {j}')
